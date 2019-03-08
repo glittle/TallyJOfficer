f90533ea5553a62540592fa837aebccfdcd90806
Glen Little <glen.little@gmail.com>
2019-03-08 08:05:05 -0700

    update icons

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,14 @@ module.exports = {
     },
 
     pwa: {
-        name: 'TallyJ Officer'
+        name: 'TallyJ Officers',
+        iconPaths: {
+            favicon32: 'img/icons/favicon-32x32.png?2',
+            favicon16: 'img/icons/favicon-16x16.png?2',
+            appleTouchIcon: 'img/icons/apple-touch-icon.png?2',
+            maskIcon: 'img/icons/safari-pinned-tab.svg?2',
+            msTileImage: 'img/icons/mstile-150x150.png?2'
+        }
     },
 
     lintOnSave: undefined
